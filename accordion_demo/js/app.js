a8d367ecb9419c746c6b045ead80609ab6cd69b8
littlewin <littlewin.wang@gmail.com>
2016-07-29 22:08:33 +0800

    Add accordion demo

---
+++ accordion_demo/js/app.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function() {
+	function close_accordion_section() {
+		jQuery('.accordion .accordion-section-title').removeClass('active');
+		jQuery('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+	}
+
+	jQuery('.accordion-section-title').click(function(e) {
+		var currentAttrValue = jQuery(this).attr('href');
+
+		if(jQuery(e.target).is('.active')) {
+			close_accordion_section();
+		}else {
+			close_accordion_section();
+			jQuery(this).addClass('active');
+			jQuery('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
+		}
+
+		e.preventDefault();
+	});
+});
