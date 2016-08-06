e014ac0bde6b45ca3d981c26c486997f2adf130a
littlewin <littlewin.wang@gmail.com>
2016-08-06 23:40:49 +0800

    update

--- canvas_countdown_demo/countdown.js
+++ canvas_countdown_demo/countdown.js
@@ -1,10 +1,13 @@
-var WINDOW_WIDTH = 1024;
+var WINDOW_WIDTH = 1920;
 var WINDOW_HEIGHT = 768;
 var RADIUS = 8;
 var MARGIN_TOP = 60;
 var MARGIN_LEFT = 30;
 const endTime = new Date(2016, 7, 7, 18, 47, 52);
 var curShowTimeSeconds = 0;
+
+var balls = [];
+const colors = ["#33B5E5","#0099CC","#AA66CC","#9933CC","#99CC00","#669900","#FFBB33","#FF8800","#FF4444","#CC0000"];
 
 window.onload = function() {
   var canvas = document.getElementById('canvas');
@@ -31,17 +34,81 @@ function getCurShowTimeSeconds() {
 
 function update() {
   var nextShowTimeSeconds = getCurShowTimeSeconds();
-  var nexthours = parseInt(nextShowTimeSeconds / 3600);
-  var nextminutes = parseInt((nextShowTimeSeconds % 3600) / 60);
-  var nextseconds = nextShowTimeSeconds % 60;
+  var nextHours = parseInt(nextShowTimeSeconds / 3600);
+  var nextMinutes = parseInt((nextShowTimeSeconds % 3600) / 60);
+  var nextSeconds = nextShowTimeSeconds % 60;
 
-  var curhours = parseInt(curShowTimeSeconds / 3600);
-  var curminutes = parseInt((curShowTimeSeconds % 3600) / 60);
-  var curseconds = curShowTimeSeconds % 60;
+  var curHours = parseInt(curShowTimeSeconds / 3600);
+  var curMinutes = parseInt((curShowTimeSeconds % 3600) / 60);
+  var curSeconds = curShowTimeSeconds % 60;
 
-  if(nextseconds != curseconds) {
+  if(nextSeconds != curSeconds) {
+    if( parseInt(curHours/10) != parseInt(nextHours/10) ){
+      addBalls( MARGIN_LEFT + 0 , MARGIN_TOP , parseInt(curHours/10) );
+    }
+    if( parseInt(curHours%10) != parseInt(nextHours%10) ){
+      addBalls( MARGIN_LEFT + 15*(RADIUS+1) , MARGIN_TOP , parseInt(curHours/10) );
+    }
+
+    if( parseInt(curMinutes/10) != parseInt(nextMinutes/10) ){
+      addBalls( MARGIN_LEFT + 39*(RADIUS+1) , MARGIN_TOP , parseInt(curMinutes/10) );
+    }
+    if( parseInt(curMinutes%10) != parseInt(nextMinutes%10) ){
+      addBalls( MARGIN_LEFT + 54*(RADIUS+1) , MARGIN_TOP , parseInt(curMinutes%10) );
+    }
+
+    if( parseInt(curSeconds/10) != parseInt(nextSeconds/10) ){
+      addBalls( MARGIN_LEFT + 78*(RADIUS+1) , MARGIN_TOP , parseInt(curSeconds/10) );
+    }
+    if( parseInt(curSeconds%10) != parseInt(nextSeconds%10) ){
+      addBalls( MARGIN_LEFT + 93*(RADIUS+1) , MARGIN_TOP , parseInt(nextSeconds%10) );
+    }
+
     curShowTimeSeconds = nextShowTimeSeconds;
   }
+
+  updateBalls();
+}
+
+function updateBalls(){
+  for( var i = 0 ; i < balls.length ; i ++ ){
+
+    balls[i].x += balls[i].vx;
+    balls[i].y += balls[i].vy;
+    balls[i].vy += balls[i].g;
+
+    if( balls[i].y >= WINDOW_HEIGHT-RADIUS ){
+      balls[i].y = WINDOW_HEIGHT-RADIUS;
+      balls[i].vy = - balls[i].vy*0.75;
+    }
+  }
+
+  var cnt = 0
+  for( var i = 0 ; i < balls.length ; i ++ )
+    if( balls[i].x + RADIUS > 0 && balls[i].x -RADIUS < WINDOW_WIDTH )
+      balls[cnt++] = balls[i]
+
+  while( balls.length > cnt ){
+    balls.pop();
+  }
+}
+
+function addBalls( x , y , num ){
+
+  for( var i = 0  ; i < digit[num].length ; i ++ )
+    for( var j = 0  ; j < digit[num][i].length ; j ++ )
+      if( digit[num][i][j] == 1 ){
+        var aBall = {
+          x: x+j*2*(RADIUS+1)+(RADIUS+1),
+          y: y+i*2*(RADIUS+1)+(RADIUS+1),
+          g: 1.5+Math.random(),
+          vx: Math.pow( -1 , Math.ceil( Math.random()*1000 ) ) * 4,
+          vy: -5,
+          color: colors[ Math.floor( Math.random()*colors.length ) ]
+        }
+
+        balls.push( aBall )
+      }
 }
 
 function render(cxt) {
@@ -60,6 +127,16 @@ function render(cxt) {
   renderDigit(MARGIN_LEFT + 69*(RADIUS+1), MARGIN_TOP, 10, cxt);
   renderDigit(MARGIN_LEFT + 78*(RADIUS+1), MARGIN_TOP, parseInt(seconds/10), cxt);
   renderDigit(MARGIN_LEFT + 93*(RADIUS+1), MARGIN_TOP, parseInt(seconds%10), cxt);
+
+  for( var i = 0 ; i < balls.length ; i ++ ){
+    cxt.fillStyle=balls[i].color;
+
+    cxt.beginPath();
+    cxt.arc( balls[i].x , balls[i].y , RADIUS , 0 , 2*Math.PI , true );
+    cxt.closePath();
+
+    cxt.fill();
+  }
 }
 
 function renderDigit(x, y, num, cxt) {
